8171f82d66c1780277f1244d95d22c5284a20230
nehabalaji <nehabalaji10@gmail.com>
2022-12-27 08:27:45 +0530

    add widgets

---
+++ app/src/main/java/com/project/theplantshack/ui/widgets/PlantItem.kt
@@ -0,0 +1,31 @@
+package com.project.theplantshack.ui.widgets
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.project.theplantshack.data.model.Plant
+import com.project.theplantshack.ui.theme.Montserrat
+import com.project.theplantshack.ui.theme.eerieBlack
+import com.project.theplantshack.ui.theme.lightGray02
+
+@Composable
+fun PlantItem(plant: Plant) {
+
+}
